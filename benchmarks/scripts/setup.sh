fb2931ec13a93cf6bc103bf5b411505ad15c6a6d
Suvorov Rodion <rodion.suvorov.94@mail.ru>
2024-11-21 14:07:46 +0000

    fix: extracting path

--- benchmarks/scripts/setup.sh
+++ benchmarks/scripts/setup.sh
@@ -7,7 +7,7 @@ echo "path: $SUITE_SPARSE_PATH/SuiteSparse"
 mkdir "$SUITE_SPARSE_PATH/SuiteSparse"
 wget -P "$SUITE_SPARSE_PATH/SuiteSparse" https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/refs/tags/v7.8.3.tar.gz
 echo "SuiteSparse version: 7.8.3"
-tar -xzf "$SUITE_SPARSE_PATH/SuiteSparse/v7.8.3.tar.gz" -C ~/Homework/SuiteSparse-GraphBLAS-Vectorization/SuiteSparse
+tar -xzf "$SUITE_SPARSE_PATH/SuiteSparse/v7.8.3.tar.gz" -C "$SUITE_SPARSE_PATH/SuiteSparse"
 mv "$SUITE_SPARSE_PATH/SuiteSparse/SuiteSparse-7.8.3/"* "$SUITE_SPARSE_PATH/SuiteSparse"
 rm -rf "$SUITE_SPARSE_PATH/SuiteSparse/SuiteSparse-7.8.3"
 rm "$SUITE_SPARSE_PATH/SuiteSparse/v7.8.3.tar.gz"
