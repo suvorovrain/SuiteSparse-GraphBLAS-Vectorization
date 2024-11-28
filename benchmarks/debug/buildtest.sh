12fe3b3d6dc8cc35c82916d0dee870f2998352ee
Suvorov Rodion <rodion.suvorov.94@mail.ru>
2024-11-28 11:29:58 +0000

    feat: add -g flag

--- benchmarks/debug/buildtest.sh
+++ benchmarks/debug/buildtest.sh
@@ -3,7 +3,7 @@
 LIBPATH=$(dirname "$(realpath "$0")")
 source "$LIBPATH/path.env"
 
-gcc -I"$GRAPH_BLAS_PATH/Include" -o "$SUITE_SPARSE_PATH/benchmarks/davx" "$SUITE_SPARSE_PATH/benchmarks/test.c" -L"$GRAPH_BLAS_PATH/build-debug-avx/" -lgraphblas -Wl,-rpath "$GRAPH_BLAS_PATH/build-debug-avx/"
-gcc -I"$GRAPH_BLAS_PATH/Include" -o "$SUITE_SPARSE_PATH/benchmarks/dnoavx" "$SUITE_SPARSE_PATH/benchmarks/test.c" -L"$GRAPH_BLAS_PATH/build-debug-noavx/" -lgraphblas -Wl,-rpath "$GRAPH_BLAS_PATH/build-debug-noavx/"
+gcc -I"$GRAPH_BLAS_PATH/Include" -o "$SUITE_SPARSE_PATH/benchmarks/davx" "$SUITE_SPARSE_PATH/benchmarks/test.c" -L"$GRAPH_BLAS_PATH/build-debug-avx/" -lgraphblas -Wl,-rpath "$GRAPH_BLAS_PATH/build-debug-avx/" -g
+gcc -I"$GRAPH_BLAS_PATH/Include" -o "$SUITE_SPARSE_PATH/benchmarks/dnoavx" "$SUITE_SPARSE_PATH/benchmarks/test.c" -L"$GRAPH_BLAS_PATH/build-debug-noavx/" -lgraphblas -Wl,-rpath "$GRAPH_BLAS_PATH/build-debug-noavx/" -g
 
 echo "Compilation completed."
