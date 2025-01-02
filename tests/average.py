34dcffb59d9ffc844f99101c04c04529f63280cb
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-01-02 17:03:52 +0300

    feat: correct table with results

--- tests/average.py
+++ tests/average.py
@@ -1,8 +1,49 @@
-import sys
 import os
 import matplotlib.pyplot as plt
 from decimal import Decimal,getcontext
-filename = sys.argv[1] 
+from matplotlib.font_manager import FontProperties
+
+
+matrices=[
+    ['olafu', 0, 0,0,0,0,0],
+    ['fd18', 0, 0,0,0,0,0],
+
+    ['sme3Da', 0, 0,0,0,0,0],
+    ['stokes64', 0, 0,0,0,0,0],
+
+    ['Goodwin_030', 0, 0,0,0,0,0],
+    ['cryg10000', 0, 0,0,0,0,0],
+
+    ['sinc12', 0, 0,0,0,0,0],
+    ['fd12', 0, 0,0,0,0,0],
+
+    ['s3rmq4m1', 0, 0,0,0,0,0],
+    ['freeFlyingRobot_12', 0, 0,0,0,0,0],
+
+    ['bcsstk15', 0, 0,0,0,0,0],
+    ['tols4000', 0, 0,0,0,0,0],
+
+    ['ex36', 0, 0,0,0,0,0],
+    ['iprob', 0, 0,0,0,0,0],
+
+    ['MISKnowledgeMap', 0, 0,0,0,0,0],
+    ['LeGresley_2508', 0, 0,0,0,0,0],
+
+    ['piston', 0, 0,0,0,0,0],
+    ['west2021', 0, 0,0,0,0,0],
+
+    ['reorientation_2', 0, 0,0,0,0,0],
+    ['netscience', 0, 0,0,0,0,0],
+
+    ['olm1000', 0, 0,0,0,0,0],
+    ['collins_15NN', 0, 0,0,0,0,0],
+
+    ['mcfe', 0, 0,0,0,0,0],
+    ['orbitRaising_3', 0, 0,0,0,0,0],
+    
+    ['tomography', 0, 0,0,0,0,0]
+]
+
 extensions =[
     'avx',
     'noavx',
@@ -13,40 +54,95 @@ global matrix_name
 global num_rows
 global num_nonzero 
 average=[]
-for extension in extensions:
-    path = (f'measurements/{extension}{filename}/{filename}.txt')
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir, path)
-    with open(path, 'r') as file:
-        data = file.read().strip().split(';')
-    matrix_name = str(data[0])
-    num_rows = int(data[1])
-    num_nonzero = int(data[2])
-    getcontext().prec=6
-    avg_time = Decimal(data[-1]) if data[-1] else None
-    average.append(avg_time)
+for filename in matrices:
+    for extension in extensions:
+        path = (f'measurements/{extension}{filename[0]}/{filename[0]}.txt')
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, path)
+        print(filename[0])
+        with open(path, 'r') as file:
+            data = file.read().strip().split(';')
+        filename[1] = int(data[1])
+        filename[2] = int(data[2])
+        avg_time = Decimal(data[-1]) if data[-1] else None
+        getcontext().prec=6
+        if extension == 'avx':
+            filename[3]= avg_time
+        elif extension == 'noavx':  
+            filename[4]= avg_time
+        elif extension == 'rvv':
+            filename[5]= avg_time
+        elif extension == 'norvv':
+            filename[6]= avg_time
+        else:
+            print("error")
+            exit
+        
+        # average.append(avg_time)
 
-print(len(average))
-plt.figure(figsize=(12, 8))
-bars =plt.bar(range(len(average)), average, edgecolor='black')
-for bar in bars:
-    height = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2., height,
-             f'{height}',
-             ha='center', va='bottom')
+matrices_name = [x[0] for x in matrices]
+matrices_rows = [x[1] for x in matrices]
+matrices_nz = [x[2] for x in matrices]
+matrices_avx = [x[3] for x in matrices]
+matrices_noavx = [x[4] for x in matrices]
+matrices_rvv = [x[5] for x in matrices]
+matrices_norvv = [x[6] for x in matrices]
 
-plt.title(f'Matrix: {filename}\n Number of rows : {num_rows}, Nonzero elements: {num_nonzero}')
-plt.xlabel('Time s.')
-plt.ylabel('Number of tests')
-plt.grid(True)
+table_data = []
+for i in range(len(matrices_name)):
+    avx_speedup = (matrices_noavx[i] - matrices_avx[i]) / matrices_noavx[i] * 100 if matrices_noavx[i] != 0 else 0
+    rvv_speedup = (matrices_norvv[i] - matrices_rvv[i]) / matrices_norvv[i] * 100 if matrices_norvv[i] != 0 else 0
 
-plt.xticks(range(len(extensions)), extensions)
+    row = [f'{matrices_name[i]}']
+    row.extend([matrices_rows[i],matrices_nz[i],matrices_avx[i], matrices_noavx[i], matrices_rvv[i], matrices_norvv[i], avx_speedup, rvv_speedup])
+    table_data.append(row)
 
-for extension in extensions:
-    path = (f'measurements/{extension}{filename}/{filename}.txt')
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir, path)
-    plot_filename = os.path.splitext(path)[0] + '_avgplot.png'
-    plt.savefig(os.path.join(current_dir, plot_filename))
-    print(f"Graph saved into: {plot_filename}\n\n")
+font = FontProperties()
+font.set_family('serif')
+font.set_name('Times New Roman')
+
+fig, ax = plt.subplots(figsize=(12, 10))
+
+table = ax.table(cellText=table_data,
+                colLabels=['Matrix name','Rows number' ,'Nonzeros' ,'AVX (s.)', 'No AVX (s.)', 'RVV (s.)', 'No RVV (s.)', 'AVX Speedup (%)', 'RVV Speedup (%)'],
+                loc='center', cellLoc='center')
+
+table.auto_set_font_size(False)
+table.set_fontsize(10)
+
+for (i, j), cell in table.get_celld().items():
+    if i == 0:
+        cell.set_text_props(fontweight='bold', horizontalalignment='center')
+        cell.set_text_props(wrap=True, font='serif', fontsize=10)
+    else:
+        cell.set_text_props(wrap=True, font='serif', fontsize=14)
+    if j == 0:
+        cell.set_width(0.13)
+        cell.set_text_props(wrap=True, font='serif', fontsize=11)
+        
+    if j == 8 and i != 0:
+        avx_speedup = float(table_data[i-1][7])
+        rvv_speedup = float(table_data[i-1][8])
+        if rvv_speedup < 0:
+            cell.set_facecolor('red')
+        elif rvv_speedup > avx_speedup:
+            cell.set_facecolor('lightgreen')
+        else:
+            cell.set_facecolor('lightcoral')
+    if  j>0:
+        cell.set_width(0.11)
+    cell.set_height(0.04)
+    cell.set_text_props()
+
+# table.scale(1.5, 1.2)
+
+ax.axis('off')
+
+# plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+
+fig.tight_layout()
+
+path = (f'measurements/avg.png')
+plt.savefig(path, dpi=300, bbox_inches='tight')
+print(f"Graph saved into: {path}\n\n")
 plt.close()
